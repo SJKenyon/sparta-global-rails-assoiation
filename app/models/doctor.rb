52acd3dc99698a9361c659dae33f6967f480a6b4
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2018-05-17 09:53:58 +0100

    Added validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   has_many :users, :through => :appointments
+  validates :name, format: { with: /\A[a-zA-Z]z/, message: "must not contain special characters" }
+  validates :age, numericality: { only_integer: true }
 end
